2e408cc12ca3ee5202605977cd6215117977586a
Yaroslav Gusev <yarigusev@gmail.com>
2025-02-26 12:46:28 +0300

    add should(be.visible) in cheking_about_company_text

--- models/pages/Information_page.py
+++ models/pages/Information_page.py
@@ -1,9 +1,9 @@
-from selene import browser, have
+from selene import browser, have, be
 
 
 class InformationPage:
     def cheking_about_company_text(self, text):
-        browser.element('#article_container_208044859').should(have.text(text))
+        browser.element('#article_container_208044859').should(be.visible).should(have.text(text))
 
     def verify_successful_search(self, text):
         browser.element('#jsx-pet-app').should(have.text(f'"{text}" найден'))
