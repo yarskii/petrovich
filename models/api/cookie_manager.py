19a15f29a02819b458f6e57042d903be69601ee3
Yaroslav Gusev <yarigusev@gmail.com>
2025-02-26 14:38:58 +0300

    add dict comprehension in cookie_manager.py

--- models/api/cookie_manager.py
+++ models/api/cookie_manager.py
@@ -4,8 +4,4 @@ from selene import browser
 class CookieManager:
     def get_browser_cookies(self):
         selenium_cookies = browser.driver.get_cookies()
-        requests_cookies = {}
-        for cookie in selenium_cookies:
-            requests_cookies[cookie['name']] = cookie['value']
-
-        return requests_cookies
+        return {cookie['name']: cookie['value'] for cookie in selenium_cookies}
